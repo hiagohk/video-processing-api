38074084dd5e8b1a76d76bc9732c0c68119bc328
Hiago Kuster <hiagohk@gmail.com>
2026-03-25 11:04:13 -0300

    Update entrypoint.worker.sh

--- docker/entrypoint.worker.sh
+++ docker/entrypoint.worker.sh
@@ -7,6 +7,10 @@ while ! nc -z postgres 5432; do
   sleep 1
 done
 
+until curl -s http://localstack:4566/_localstack/health | grep '"sqs": "running"'; do
+  sleep 2
+done
+
 echo "Starting worker..."
 
 exec python app/worker/worker.py
